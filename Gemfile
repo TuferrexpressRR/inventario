c5d348ccd17b57ac50ca75384e187b016f05ea8a
guimelrivas <108141792+guimelrivas@users.noreply.github.com>
2022-08-11 12:13:46 -0400

    ActualizandoUrgente
    ActualizandoUrgente

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem "sprockets-rails"
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', group: :test
-gem 'pg', group: :production
+# gem 'pg', group: :production
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
@@ -75,3 +75,5 @@ end
 gem 'rubyzip', '>= 1.2.1'
 gem 'axlsx'
 gem 'axlsx_rails'
+
+gem 'devise'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_052406) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_11_154532) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "products_id", null: false
+    t.integer "cart_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["products_id"], name: "index_line_items_on_products_id"
+  end
+
   create_table "movements", force: :cascade do |t|
     t.integer "product_id", null: false
     t.integer "movement_type"
@@ -30,5 +44,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_052406) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "products", column: "products_id"
   add_foreign_key "movements", "products"
 end
